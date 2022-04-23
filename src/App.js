e6901fad3d9d9d6eb35b26ceb8fe284b4a7723a2
Marijo Radman <marijo.radman@gmail.com>
2022-04-23 09:58:14 +0200

    first big update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "components/Header";
+import { useAuthUser } from "lib/firebase";
+import CreatePost from "pages/CreatePost";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Signup from "pages/Signup";
+import { CreateFlow } from "./components/SuperFluid/CreateFlow"
+import { Route, Switch } from "react-router-dom";
+
+export default function App() {
+  useAuthUser()
+
+  return <>
+  <Route component={Header} />
+  <Switch>
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/createjob" component={CreateFlow} />
+    <Route path="/createpost" component={CreatePost} />
+    <Route path="/" component={Home} />
+
+
+
+  </Switch>
+  </>;
+}
---
+++ src/components/CommentList/index.js
@@ -0,0 +1,17 @@
+import CommentListItem from "components/CommentList/Item";
+import styled from "styled-components/macro";
+
+const List = styled.ul`
+  margin-top: 16px;
+  list-style: none;
+`;
+
+export default function CommentList({ comments }) {
+  return (
+    <List>
+      {comments.map((comment) => (
+        <CommentListItem key={comment.id} comment={comment} />
+      ))}
+    </List>
+  );
+}
