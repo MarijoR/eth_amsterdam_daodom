68006b7969992bbd34cecab45b3cfc671f6a36d2
RocioSe <81631318+RocioSe@users.noreply.github.com>
2022-07-21 12:52:57 +0200

    Gnosis create factory 🏭

--- src/pages/Gnosis.js
+++ src/pages/Gnosis.js
@@ -1,71 +1,162 @@
 import { React, useState, useEffect } from "react";
 import { Box, Button, Card, Grid, Typography } from "@mui/material";
-import { ethers } from 'ethers'
-import {EthersAdapter} from '@gnosis.pm/safe-core-sdk'
-import Safe, { SafeFactory, SafeAccountConfig } from '@gnosis.pm/safe-core-sdk'
+import { ethers } from "ethers";
+//import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
+//import Safe, { SafeFactory, SafeAccountConfig } from "@gnosis.pm/safe-core-sdk";
 //import { ContractNetworksConfig } from '@gnosis.pm/safe-core-sdk'
 import { useMoralis } from "react-moralis";
-import { ContractNetworksConfig } from '@gnosis.pm/safe-core-sdk'
-
+//import SafeServiceClient from "@gnosis.pm/safe-service-client";
+import Safe, { SafeFactory, EthersAdapter } from "@gnosis.pm/safe-core-sdk";
 
 export default function Gnosis() {
-
   const [safeFactory, setSafeFactory] = useState("");
   const [safe, setSafe] = useState("");
+  const [safeSdk, setSafeSdk] = useState("");
   const [safeaddress, setSafeAddress] = useState("");
 
+  const owners = [
+    "0x063e750Ab7F6bC6d70B7d18A170D0771a6D78927",
+    "0x3E6f83CAd0B7af12838dbe3d8b7aaaA11FE02FB5",
+    "0xfEEB45B502EaEF26f6a5d1918e67406D41404ad0",
+  ];
+  const threshold = 3;
 
-    const createSafe = async () =>{
-    console.log("test");
+  const MULTI_SEND_ADDRESS = "0xA470120a5B2875842b9Cf8a2e342beDC6b7DBe4E";
+  const MASTER_COPY_ADDRESS = "0x5a05f7003a11612687b664718FC3AD1D2ee900da";
+  const PROXY_FACTORY_ADDRESS = "0x82FAC758b3670A82BF7F815db6291c151DE023cE";
 
-    const web3Provider = window.ethereum;
+  async function createSafe(owners, threshold) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const safeOwner = provider.getSigner(0);
+    const adapter = new EthersAdapter({ ethers, signer: safeOwner });
+    const id = await adapter.getChainId();
+    try {
+      const contractNet = {
+        [id]: {
+          multiSendAddress: MULTI_SEND_ADDRESS,
+          safeMasterCopyAddress: MASTER_COPY_ADDRESS,
+          safeProxyFactoryAddress: PROXY_FACTORY_ADDRESS,
+        },
+      };
 
-  //const web3Provider = web3.currentprovider;
-  const provider = new ethers.providers.Web3Provider(web3Provider)
-  //const provider = new ethers.providers.Web3Provider(web3Provider);
-  const safeOwner = provider.getSigner(0);
+      try {
+        console.log("ethAdapter:", adapter);
+        const factory = await SafeFactory.create({
+          ethAdapter: adapter,
+          contractNetworks: contractNet,
+        });
+        console.log("factory: ", factory);
+        setSafeFactory(factory);
+        if (!safeaddress) return;
 
-    const ethAdapter = new EthersAdapter({
-      ethers,
-      signer: safeOwner
-      })
-    const id = await ethAdapter.getChainId()
+        const safeSdk = await Safe.create({
+          ethAdapter: adapter,
+          safeaddress /*, contractNetworks*/,
+        });
 
-    const safeSdk = await Safe.deploy({ ethAdapter, safeaddress})
-    const safeFactory =  await SafeFactory.create({ ethAdapter, safeaddress });
-    setSafeFactory(safeFactory());
-    console.log("test2")
-    //const safeSdk  =  await Safe.deploySafe({ safeAccountConfig, });  
-    setSafe(safeSdk());
-    console.log("test3")
-    setSafeAddress(safeSdk.getAddress());
-    console.log(safeaddress);
-    console.log(safe);
+        setSafeSdk(safeSdk);
+      } catch (error) {
+        console.error("error:", error);
+      }
+
+      console.log(safeFactory);
+      if (!safeFactory) return;
+      console.log("SI HAY UNA safefactory");
+
+      const safeAccountConfig = { owners, threshold };
+      console.log(
+        "CONFIGURACION DEL SAFE CON LAS VARIABLE OWNER Y THRESHOLD: safeAccountConfig",
+        safeAccountConfig
+      );
+      let safe;
+      try {
+        safe = await safeFactory.deploySafe(safeAccountConfig);
+        console.log("CONS SAFE DESPUES DE HABER LLAMADO DEPLOYSAFE:", safe);
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+      const newSafeAddress = ethers.utils.getAddress(safe.getAddress());
+      setSafeAddress(newSafeAddress);
+      console.log("newSafeAddress: ", newSafeAddress);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  /*  const createSafe1 = async () => {
+    console.log("test");
+    //const provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log("provider: ", provider);
+
+    //const signer = provider.getSigner();
+    console.log("signer: ", signer);
+   // const safeOwner = provider.getSigner(0);
+    console.log("safeOwner: ", safeOwner);
+
+//    const ethAdapter = new EthersAdapter({ ethers, signer: safeOwner });
+    console.log("ethAdapter: ", ethAdapter);
+
+    const id = await ethAdapter.getChainId();
+    console.log("id: ", id);
+    console.log("Safe: ", Safe);
+    console.log("SafeFactory: ", SafeFactory.create);
+
+    const txServiceUrl = "https://safe-transaction.gnosis.io";
+    const safeService = new SafeServiceClient({ txServiceUrl, ethAdapter });
+    console.log("safeService: ", safeService);
+
+    try {
+      const safeFactory = await SafeFactory.create({ ethAdapter });
+      console.log("safeFactory: ", safeFactory);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+
+    /*     const safeSdk = await Safe.create({ ethAdapter, safeaddress });
+    console.log("safeSdk: ", safeSdk); */
+
+  /* const safeSdk = await Safe.deploy({ ethAdapter, safeaddress });
+
+    const safeFactory = await SafeFactory.create({ ethAdapter, safeaddress });
+*/
+  /* setSafeFactory(safeFactory());
+    console.log("test2");
+    //const safeSdk  =  await Safe.deploySafe({ safeAccountConfig, });
+    setSafe(safeSdk());
+    console.log("test3");
+    setSafeAddress(safeSdk.getAddress());
+    console.log(safeaddress);
+    console.log(safe); 
+  }; */
 
   //const safeFactory = async () => await SafeFactory.create({ ethAdapter });
 
-  const owners = ['0x9bfB07106c0Ce48B037aFb0757dd1C5eb94b20cA', '0x8Bc34a0823bFF3D1dF739E135aD2e332a9565C1b', '0x088006AFcD8130fd2AC61662c6c98E9B2e933Df3']
-  const threshold = 3
-  const safeAccountConfig  = {
-  owners,
-  threshold,
-  };
+  /* const owners = [
+    "0x063e750Ab7F6bC6d70B7d18A170D0771a6D78927",
+    "0x3E6f83CAd0B7af12838dbe3d8b7aaaA11FE02FB5",
+    "0xfEEB45B502EaEF26f6a5d1918e67406D41404ad0",
+  ];
+  const threshold = 3; 
+  const safeAccountConfig = {
+    owners,
+    threshold,
+  };*/
 
-  
-//const safeSdk = async () => await safeFactory.deploySafe({ safeAccountConfig });
+  //const safeSdk = async () => await safeFactory.deploySafe({ safeAccountConfig });
 
-//const newSafeAddress = safeSdk.getAddress();
+  //const newSafeAddress = safeSdk.getAddress();
 
-{/*const getSafeAddress = async () =>{
+  /* {
+    const getSafeAddress = async () =>{
   const newSafeAddress = safeSdk.getAddress();
   setSafe(newSafeAddress);
-}*/}
+}
+  }*/
 
-//const ethAdapterOwner1 = '0x9bfB07106c0Ce48B037aFb0757dd1C5eb94b20cA';
-//const safeAddress = '0x9bfB07106c0Ce48B037aFb0757dd1C5eb94b20cA';
-//const safeSdkRetry = async () => await Safe.create({ ethAdapter: ethAdapterOwner1, safeAddress });
+  //const ethAdapterOwner1 = '0x9bfB07106c0Ce48B037aFb0757dd1C5eb94b20cA';
+  //const safeAddress = '0x9bfB07106c0Ce48B037aFb0757dd1C5eb94b20cA';
+  //const safeSdkRetry = async () => await Safe.create({ ethAdapter: ethAdapterOwner1, safeAddress });
 
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState(null);
@@ -77,39 +168,46 @@ export default function Gnosis() {
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [balance, setBalance] = useState("");
 
-  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+  const {
+    authenticate,
+    isAuthenticated,
+    isAuthenticating,
+    user,
+    account,
+    logout,
+  } = useMoralis();
 
-    useEffect(() => {
+  useEffect(() => {
     if (isAuthenticated) {
       // add your logic here
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated]);
 
-    const login = async () => {
-      if (!isAuthenticated) {
-
-        await authenticate({signingMessage: "Log in using Moralis" })
-          .then(function (user) {
-            console.log("logged in user:", user);
-            console.log(user.get("ethAddress"));
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({ signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
+  };
 
-    const logOut = async () => {
-      await logout();
-      console.log("logged out");
-    }
-
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  };
 
   return (
     <div>
       <button onClick={login}>Moralis Metamask Login</button>
-      <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+      <button onClick={logOut} disabled={isAuthenticating}>
+        Logout
+      </button>
       <Box sx={{ flexGrow: 1 }} margin={2}>
         {isAuthenticated ? (
           <Grid
@@ -129,16 +227,16 @@ export default function Gnosis() {
               >
                 <Typography margin={3} variant="h5" color="#345DA7">
                   {" "}
-                Meine Wallet Adresse:
+                  Meine Wallet Adresse:
                 </Typography>
 
                 <Typography variant="h6">{user.get("ethAddress")}</Typography>
 
                 <Typography margin={3} variant="h5" color="#345DA7">
                   {" "}
-                Meine Safe Adresse:
+                  Meine Safe Adresse:
                 </Typography>
-                
+
                 <Typography variant="h6">{safeaddress}</Typography>
 
                 <Button
@@ -178,8 +276,7 @@ export default function Gnosis() {
                 color="primary"
                 fontFamily={"sans-serif"}
               >
-                Daily should never be missed -
-                Marijo
+                Daily should never be missed - Marijo
               </Typography>
             </Grid>
           </Grid>
